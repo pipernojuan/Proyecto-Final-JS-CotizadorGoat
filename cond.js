aae1208e5338419c3fa6e3f3481077b789d049e8
piperno <juan.piperno@gmail.com>
2022-08-08 17:18:35 -0300

    v1

---
+++ cond.js
@@ -0,0 +1,23 @@
+const swa = () => {
+    Swal.fire({
+        title: '¡No te olvides!',
+        text: 'Seleccioná los valores faltantes para poder cotizar',
+        icon: 'warning',
+        timer: 8000,
+        timerProgressBar: true,
+    })
+}
+
+function swalert(e){
+    console.log
+    e.preventDefault();
+    // objBusqueda está en app.js: 
+    const {moneda, criptomoneda} = objBusqueda;
+    if (moneda === '' || criptomoneda === '') {
+        swa ();
+    }
+    else{consultarAPI(moneda, criptomoneda);
+    //console.log(moneda);
+    //console.log(criptomoneda);
+    }
+}
